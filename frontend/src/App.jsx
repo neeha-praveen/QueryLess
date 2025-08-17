3cb044437ecf261baffa4ef13def803086c9ce0d
neeha-praveen <neeha.npraveen@gmail.com>
2025-08-17 20:25:35 +0530

    Modify App.jsx, App.css, Sidebar.css

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,11 +2,15 @@ import { useState } from 'react'
 import './App.css'
 import Navbar from './components/Navbar/Navbar'
 import Sidebar from './components/Sidebar/Sidebar'
+import Header from './components/Header/Header'
 
 function App() {
   return (
     <div className='app'>
       <Sidebar/>
+      <div className="main-body">
+        <Header/>
+      </div>
     </div>
   )
 }
